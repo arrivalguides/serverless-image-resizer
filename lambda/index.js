460117dec935ee81cc95a0bb0daa89c517ea3f09
oleksiy.zubovskyy <bashconsole@gmail.com>
2018-02-27 12:55:09 +0200

    enhanced error handling and image processing

--- lambda/index.js
+++ lambda/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const S3 = new AWS.S3({
-  signatureVersion: 'v4',
-});
+const S3 = new AWS.S3({ signatureVersion: 'v4' });
 const Sharp = require('sharp');
-const Path = require("path");
+const Url = require('url');
 
 
 const BUCKET_SOURCE = process.env.BUCKET_SOURCE;
@@ -26,21 +24,49 @@ if (process.env.ALLOWED_EXTENSIONS) {
 }
 
 exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/((\d+)x(\d+))\/(.*)/);
+  
+  let key = event.queryStringParameters.key;
+
+  // If we don't have key, that contain path to image, then we can't continue
+  if (key === undefined) {
+    return callback(null, {
+      statusCode: '400',
+      body: JSON.stringify({error: 'Key does not exists.'}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  const match = key.match(/((\d+)x?(\d+)?\/)?(.+\.(png|jpg|jpeg|tif|tiff|webp))/);
+
+  if (match === null) {
+    // URL don't match regexp
+    return callback(null, {
+      statusCode: '400',
+      body: JSON.stringify({ error: 'Key does not match form: Nx?N?/name.[jpeg|jpg|png|tiff|webp]. Not supported image format.' }),
+      headers: {
+            'Content-Type': 'application/json'
+      }
+    });
+  }
+  
+  
   const dimensions = match[1];
   const width = parseInt(match[2], 10);
   const height = parseInt(match[3], 10);
   const originalKey = match[4];
-  const originalFilename = Path.basename(originalKey);
-  const originalExtension = Path.extname(originalKey);
+  const originalExtension = match[5];
 
+  
   
   if(ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
      callback(null, {
         statusCode: '403',
-        headers: {},
-        body: '',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        body: JSON.stringify({ error: 'Not supported image dimensions.' }),
     });
     return;
   }
@@ -48,30 +74,93 @@ exports.handler = function(event, context, callback) {
     if(ALLOWED_EXTENSIONS.size > 0 && !ALLOWED_EXTENSIONS.has(originalExtension)) {
      callback(null, {
         statusCode: '403',
-        headers: {},
-        body: '',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        body: JSON.stringify({ error: 'Not supported image extension.' }),
     });
     return;
   }
   
+  const supportWebP = (event.headers.Accept.indexOf('webp') > -1);
+  const parsedURL = Url.parse(event.queryStringParameters.key, true, true);
+  
+  // If browser support webp — redirect to webp
+  if (supportWebP && originalExtension !== 'webp') {
+    const extensionFix = RegExp(/.(jpeg|jpg|png|tiff|webp)/,'ig');
+    key = key.replace(extensionFix, '.webp');
+  }
+  
 
-  S3.getObject({
-            Bucket: BUCKET_SOURCE,
-            Key: originalKey
-      }).promise().then(data => Sharp(data.Body)
-            .resize(width, height)
-//          .toFormat('png')
-            .toBuffer()
-    ).then(buffer => S3.putObject({
-            Body: buffer,
-            Bucket: BUCKET_TARGET,
-            ContentType: buffer.ContentType,
-            Key: key,
-      }).promise()
-    ).then(() => callback(null, {
-            statusCode: '301',
-            headers: {'location': `${URL}/${key}`},
-            body: '',
-      })
-    ).catch(err => callback(err))
+  // Let get metadata by key
+  S3.headObject({Bucket: BUCKET_SOURCE, Key: originalKey}, function(err, data) {
+
+    if (err) {
+      // Can't find this file
+      return callback(null, {
+        statusCode: '404',
+        body: JSON.stringify({
+            error: 'Key does not exists.'
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    }
+    
+    S3.getObject({Bucket: BUCKET_SOURCE, Key: originalKey}).promise().then((data) => {
+      let image = Sharp(data.Body);
+      return image.metadata().then((metadata) => {
+
+        // Let's calculate image size
+        // 0 minimal
+        // 5000 maximal
+        // But no upscale
+        // Aspect ratio maintained
+        let width = parsedURL.query.width ? parsedURL.query.width : (match[2] === undefined) ? metadata.width : parseInt(match[2], 10);
+        let height = parsedURL.query.height ? parsedURL.query.height : (match[3] === undefined) ? null : parseInt(match[3], 10);
+
+        let isHeight = (height !== null);
+        let ratio = isHeight ? width / height : metadata.width / metadata.height;
+        let targetWidth = Math.floor(Math.max(0, Math.min(Math.min(parseInt(width, 10), metadata.width), 5000)));
+        let targetHeight = Math.floor(Math.max(0, Math.min(targetWidth / ratio, 5000)));
+
+        if (supportWebP) {
+          return image
+            .resize(targetWidth, targetHeight)
+            .crop(Sharp.gravity.north)
+            .webp({
+                quality: 90,
+                force: true
+            })
+            .toBuffer();
+        }
+        
+        return image
+          .resize(targetWidth, targetHeight)
+          .crop(Sharp.gravity.north)
+          .jpeg({
+                quality: 90, 
+                chromaSubsampling: '4:4:4'
+          })
+          .toBuffer();
+
+      });
+    }).then(buffer => S3.putObject({
+      Body: buffer,
+      Bucket: BUCKET_TARGET,
+      ContentType: supportWebP ? 'image/webp' : 'image/jpeg',
+      Key: key,
+      Tagging: "resized=true"
+    }).promise()).then(() => callback(null, {
+      statusCode: '301',
+      headers: {
+        'location': `${URL}/${key}`
+      },
+      body: ''
+    })).catch(err => callback(err));
+  });
+    
+    
+    
 }
